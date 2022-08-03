cda39429dcc9560594bcdee693cb5f74fb901597
Nufunello <nafania578@gmail.com>
2022-08-03 21:58:45 +0300

    Content Grid

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+class Size {
+  late final double height;
+  late final double width;
+  Size(this.width, this.height);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Preferences {
+  late Size itemSize;
+  Preferences(this.itemSize);
+}
 
-  // This widget is the root of your application.
+class ContentPage extends StatefulWidget {
+  final Preferences _preferences;
+  const ContentPage(this._preferences, {Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => ContentState();
+}
+
+class ContentState extends State<ContentPage> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo');
+    final size = widget._preferences.itemSize;
+    return Center(
+        child: CustomScrollView(slivers: [
+      SliverGrid(
+        delegate: SliverChildBuilderDelegate((context, index) {
+          return Text('$index');
+        }),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            mainAxisExtent: size.height, maxCrossAxisExtent: size.width),
+      )
+    ]));
   }
+}
+
+void main() {
+  var preferences = Preferences(Size(150, 150));
+  runApp(MaterialApp(
+    title: "Whatever cloud",
+    home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+            child: ContentPage(
+          preferences,
+        ))),
+  ));
 }
