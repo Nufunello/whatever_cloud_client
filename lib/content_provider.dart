53063955a1ec30d613026f4bcb04eba8afbb291d
Nufunello <nafania578@gmail.com>
2022-08-13 16:24:15 +0300

    New API with size

--- lib/content_provider.dart
+++ lib/content_provider.dart
@@ -14,11 +14,17 @@ class Item {
   const Item({required this.image, required this.title});
 }
 
+class ContextItem extends ChangeNotifier {}
+
 class Context {
-  final Client client;
   static const _exchange = 'amq.direct';
-  static const ExchangeType type = ExchangeType.DIRECT;
-  var content = <int, Completer<Item>>{};
+  static const ExchangeType _type = ExchangeType.DIRECT;
+
+  final Client client;
+  Stream<int> get count => _countController.stream;
+
+  final _countController = StreamController<int>();
+  final _content = <int, Completer<Item>>{};
 
   void _prepareConsumer(Consumer consumer) {
     consumer.queue.purge();
@@ -27,41 +33,61 @@ class Context {
   void _requestContent(int index, int size) {
     client
         .channel()
-        .then((channel) => channel.exchange('amq.direct', type, durable: true))
-        .then((exchange) => {
-              exchange.publish(
-                  {'Index': index, 'Size': size}, "whatever_cloud/server"),
-            });
+        .then((channel) => channel.exchange('amq.direct', _type, durable: true))
+        .then(
+          (exchange) => exchange.publish({'Index': index, 'Size': size},
+              "whatever_cloud/server/home/content"),
+        );
   }
 
-  void _listener(AmqpMessage message) {
+  void _requestSize() {
+    client
+        .channel()
+        .then((channel) => channel.exchange('amq.direct', _type, durable: true))
+        .then(
+          (exchange) => exchange.publish("home", "whatever_cloud/server/size"),
+        );
+  }
+
+  void _content_listener(AmqpMessage message) {
     final Iterable payload = json.decode(message.payloadAsString);
     for (var json in payload) {
+      final completer = _content[int.parse(json['Index'])]!;
       final item = Item(
           image: Image.file(File(
               'C:/Users/nafan/Desktop/dev/whatever_cloud_client/build/owl.jpg')),
           title: json['Title']);
-      final completer = content[int.parse(json['Index'])]!;
       completer.complete(item);
     }
+  }
+
+  void _size_listener(AmqpMessage message) {
+    _countController.add(message.payloadAsJson['Size']);
   }
 
   Context({required this.client}) {
     client
         .channel()
-        .then((channel) => channel.exchange(_exchange, type, durable: true))
-        .then((exchange) =>
-            exchange.bindQueueConsumer('home', ['whatever_cloud/client']))
-        .then((consumer) {
-      _prepareConsumer(consumer);
-      consumer.listen(_listener);
+        .then((channel) => channel.exchange(_exchange, _type, durable: true))
+        .then((exchange) {
+      exchange.bindQueueConsumer('home_content',
+          ['whatever_cloud/client/home/content']).then((consumer) {
+        _prepareConsumer(consumer);
+        consumer.listen(_content_listener);
+      });
+      exchange.bindQueueConsumer(
+          'home_size', ['whatever_cloud/client/home/size']).then((consumer) {
+        _prepareConsumer(consumer);
+        consumer.listen(_size_listener);
+      });
     });
+    _requestSize();
   }
   Future<Item> getItem(int index) {
-    if (content.containsKey(index)) {
-      return content[index]!.future;
+    if (_content.containsKey(index)) {
+      return _content[index]!.future;
     } else {
-      final completer = content.putIfAbsent(index, () => Completer());
+      final completer = _content.putIfAbsent(index, () => Completer());
       var future = completer.future;
       _requestContent(index, 1);
       return future;
@@ -71,7 +97,7 @@ class Context {
   Future<void> preload(int index, int size) async {
     final futures = List<Future<Item>>.generate(size, (i) {
       i + index;
-      var future = content.putIfAbsent(i, () => Completer()).future;
+      var future = _content.putIfAbsent(i, () => Completer()).future;
       _requestContent(i, 1);
       return future;
     });
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'content_provider.dart' as content;
 
@@ -13,8 +15,11 @@ class Preferences {
 }
 
 class ContentPage extends StatefulWidget {
-  final Preferences _preferences;
-  const ContentPage(this._preferences, {Key? key}) : super(key: key);
+  final Preferences preferences;
+  final Stream<content.Context> context;
+  const ContentPage(
+      {Key? key, required this.preferences, required this.context})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => ContentState();
@@ -30,21 +35,41 @@ class ContentPageItem extends StatelessWidget {
 }
 
 class ContentState extends State<ContentPage> {
+  content.Context? _contentContext;
+  int _count = 0;
+
+  void _bind_context(content.Context event) {
+    setState(() => _contentContext = event);
+    event.count.listen((event) {
+      setState(() => _count = event);
+    });
+  }
+
+  FutureBuilder<content.Item> item(Future<content.Item> future) {
+    return FutureBuilder<content.Item>(
+      builder: ((context, snapshot) {
+        if (snapshot.hasData) {
+          return ContentPageItem(item: snapshot.requireData);
+        } else {
+          return const CircularProgressIndicator();
+        }
+      }),
+      future: future,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final size = widget._preferences.itemSize;
+    if (_contentContext == null) {
+      widget.context.listen(_bind_context);
+    }
+    final size = widget.preferences.itemSize;
     return Center(
         child: CustomScrollView(slivers: [
       SliverGrid(
-        delegate: SliverChildBuilderDelegate((context, index) {
-          final item = content.ItemProvider().getContext("home").getItem(index);
-          return FutureBuilder<content.Item>(
-            builder: ((context, snapshot) => (snapshot.hasData)
-                ? ContentPageItem(item: snapshot.requireData)
-                : const CircularProgressIndicator()),
-            future: item,
-          );
-        }),
+        delegate: SliverChildBuilderDelegate(
+            childCount: _count,
+            (context, index) => item(_contentContext!.getItem(index))),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             mainAxisExtent: size.height, maxCrossAxisExtent: size.width),
       )
@@ -54,14 +79,15 @@ class ContentState extends State<ContentPage> {
 
 void main() async {
   var preferences = Preferences(Size(150, 150));
-  content.ItemProvider().getContext("home").preload(0, 10);
+  final contextController = StreamController<content.Context>();
+  contextController
+      .add(content.ItemProvider().getContext("home")..preload(0, 10));
   runApp(MaterialApp(
     title: "Whatever cloud",
     home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
             child: ContentPage(
-          preferences,
-        ))),
+                preferences: preferences, context: contextController.stream))),
   ));
 }
